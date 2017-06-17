ddefc7d02d025af7abda02fe27fcaceefd534c9b
T- <T-@PC188>
2017-06-16 21:39:45 -0500

    Java

--- src/main/java/org/madrid/bimestral/ControladorDireccion.java
+++ src/main/java/org/madrid/bimestral/ControladorDireccion.java
@@ -5,6 +5,7 @@
  */
 package org.madrid.bimestral;
 
+import java.util.ArrayList;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -33,4 +34,32 @@ public class ControladorDireccion {
                 @PathVariable Long cp, @PathVariable String municipio){
             return repo.save(new Direccion(numero,calle,cp,municipio));
         }
+  
+@RequestMapping(value="/direccion/{numero}/{calle}/{cp}/{municipio}",
+        method=RequestMethod.PUT,headers={"Accept=application/json"})
+        public Direccion guardar(@PathVariable Long id, @PathVariable Integer numero,@PathVariable String calle,
+                @PathVariable Long cp, @PathVariable String municipio){
+            return repo.save(new Direccion(id,numero,calle,cp,municipio));
+        }
+@RequestMapping(value="/direccion/{id}",
+        method=RequestMethod.DELETE,headers={"Accept=application/json"})
+        public Estatus guardar(@PathVariable Long id){
+            repo.delete(new Direccion(id));
+            Estatus estatus = new Estatus();
+            estatus.setSuccess(true);
+            return estatus;
+        }
+        
+@RequestMapping(value="/direccion", method=RequestMethod.GET,
+        headers={"Accept=application/json"})
+public ArrayList<Direccion> buscarTodos(){
+    return (ArrayList<Direccion>) repo.findAll();
+
+}
+
+@RequestMapping(value="/direccion/{id}", method=RequestMethod.GET,
+        headers={"Accept=application/json"})
+public Direccion buscarPorId(@PathVariable Long id){
+    return repo.findOne(id);
+}
 }
--- src/main/resources/public/js/direccion.js
+++ src/main/resources/public/js/direccion.js
@@ -1,12 +1,46 @@
-//vamos a hacer el request a nuestro servicio usuando ajax con jquery
-$("#guardarDireccion").click(function() {
-    //Aplicando el metodo post usando la uri del servicio
-var numero=$("#numero").val();
-var calle=$("#calle").val();
-var cp=$("#cp").val();
-var municipio=$("#municipio").val();
-$.post("direccion/"+numero+'/'+calle+'/'+cp+'/'+municipio,function(json){
-console.log(json.id);
-});
+//Buscar todos
+
+$.ajax({
+    url:'direccion',
+    type: 'get',
+    success: function(json){
+console.log('Numero de elementos encontrados' +json.length);
+for(var i=0;i<json.length;i++){
+$("#elementos").append("<li>" + json[i].municipio+"</li>");
+$("#tablita").append("<tr><td>"+json[i].id+"</td><td>"+json[i].numero+"</td><td>"+json[i].calle+"</td><td>"+json[i].cp+"</td><td>"+json[i].municipio+"</td></tr>")
 }
-);
+}
+});
+
+
+
+//vamos a hacer el request a nuestro servicio usuando ajax con jquery
+$("#guardarDireccion").click(function(){
+            //Aplicamos el metodo post usando la uri del servicio
+            var numero=$("#numero").val();
+            var calle=$("#calle").val();
+            var cp=$("#cp").val();
+            var municipio=$("#municipio").val();
+            
+          $.post("direccion/"+numero+'/'+calle+'/'+cp+'/'+municipio,function(json){
+              console.log(json.id);
+               })  ;
+            
+           }
+        );
+
+$("#buscarPorId").click(function(){
+            //Aplicamos el metodo post usando la uri del servicio
+            var id=$("#id").val();
+      
+            
+          $.get("direccion/"+id,function(json){
+              console.log(json.municipio);
+              $("#numero2").val(json.numero);
+              $("#calle2").val(json.calle);
+              $("#cp2").val(json.cp);
+              $("#municipio2").val(json.municipio);
+               })  ;
+            
+           }
+        );
