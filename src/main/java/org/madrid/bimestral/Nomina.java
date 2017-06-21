39a93ae3572352e95852ab9dd151cee2214210c5
T- <T-@PC188>
2017-06-20 21:45:50 -0500

    Java

---
+++ src/main/java/org/madrid/bimestral/Nomina.java
@@ -0,0 +1,15 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.madrid.bimestral;
+
+/**
+ *
+ * @author T-
+ */
+public interface Nomina {
+
+public String pagarNomina();
+}
